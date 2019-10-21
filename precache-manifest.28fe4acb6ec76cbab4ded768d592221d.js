c79d9f386b956c0232e0959785100909361c551b
homura <homura.dev@gmail.com>
2019-10-21 22:59:01 +0800

    Updates

---
+++ precache-manifest.28fe4acb6ec76cbab4ded768d592221d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e8086ebc1f53d54d6984",
+    "url": "/static/js/main.8f63392f.chunk.js"
+  },
+  {
+    "revision": "7985b106ebc4dacdc7e0",
+    "url": "/static/js/2.5399c60c.chunk.js"
+  },
+  {
+    "revision": "e8086ebc1f53d54d6984",
+    "url": "/static/css/main.41e83c1d.chunk.css"
+  },
+  {
+    "revision": "7985b106ebc4dacdc7e0",
+    "url": "/static/css/2.0b083882.chunk.css"
+  },
+  {
+    "revision": "acf8c4989053ddc9060923121865033e",
+    "url": "/index.html"
+  }
+];
